14fb96e1089868ec069f9d8876951edc563d6a35
mamellal <94757188+Mohamed-Amellal@users.noreply.github.com>
2026-09-19 18:32:38 +0100

    Refine docstring in ft_filter functiontest
    Updated docstring to clarify function behavior.

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,7 +1,7 @@
 def ft_filter(function, iterable):
-    """Recode of the built-in filter using a list comprehension.
-    Return the items of iterable for which function(item) is true. When
-    function is None, keep the items that are truthy.
+    """Return an iterator yielding those items of iterable for which
+    function(item) is true.  If function is None, return the items that
+    are true.
     """
     if function is None:
         return [item for item in iterable if item]
